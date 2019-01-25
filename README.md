d5417ef5aa89631cdd1069e28cc497d80ed898ac
xqx <1025529920@qq.com>
2019-01-25 10:46:21 +0800

    better-scroll组件

---
+++ README.md
@@ -0,0 +1,21 @@
+# better-scroll
+
+> 封装一个better-scroll组件，包括上拉刷新，下拉刷新效果。有借鉴他人
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="index">
+    <h2 class="title">封装组件better-scroll滚动<br/>  以及上拉下拉刷新</h2>
+    <h2 class="text">列表</h2>
+    <scroll class="box" :data="list" :pulldown="pulldown" @pulldown="loadData">
+        <ul>
+          <li v-for="(item,index) in list" :key="index">
+            <img :src="item.img" alt="">
+          </li>
+        </ul>
+    </scroll>
+  </div>
+</template>
+
+<script>
+import Scroll from '@/base/scroll/scroll'
+export default {
+  name: 'index',
+  data () {
+    return {
+      list:[{
+          id:1,
+          img:'http://img3.imgtn.bdimg.com/it/u=2957286837,1770710086&fm=26&gp=0.jpg'
+        },{
+          id:2,
+          img:'http://img2.imgtn.bdimg.com/it/u=1206685234,500217256&fm=26&gp=0.jpg'
+        },{
+          id:3,
+          img:'http://img4.imgtn.bdimg.com/it/u=1843568763,107432329&fm=26&gp=0.jpg'
+        }
+      ]
+    }
+  },
+  components: {
+    Scroll
+  },
+  methods: {
+    loadData () {
+      // 调用api获取数据
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.index {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  >.title {
+    text-align: center;
+    font-size: 18px;
+  }
+  .text {
+    font-size: 16px;
+    line-height: 30px;
+    padding-left: 10px;
+  }
+  ul {
+    width: 100%;
+    overflow: hidden;
+    li {
+      width: 100%;
+      padding: 10px;
+      box-sizing: border-box;
+      >img {
+        width: 100%;
+        height: 500px;
+      }
+    }
+  }
+}
+</style>
+
